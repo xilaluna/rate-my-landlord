b7bae48ec416134abc1bda7fda221698c1b85874
Xila Luna <xilaluna2@gmail.com>
2022-10-14 23:11:40 -0700

    style: added prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,12 @@
 /** @type {import("prettier").Config} */
 module.exports = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };

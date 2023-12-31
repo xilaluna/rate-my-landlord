eaa809388d5fdfbb647c114efb8da22eca8e4ba5
xilaluna <xilaluna2@gmail.com>
2023-12-30 21:01:40 -0800

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getPosts } from "~/lib/post";
 export default async function HomePage() {
   const posts = await getPosts();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main>
       {posts.map((post) => (
         <div key={post.id} className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">{post.name}</h1>

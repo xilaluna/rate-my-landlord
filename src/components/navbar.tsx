681dc58e3b575e11766ebfbc18f83a0100ac44c3
xilaluna <xilaluna2@gmail.com>
2024-01-04 01:07:28 -0800

    update nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 border-b md:px-5">
-      <a className="btn btn-ghost text-xl">Rate My Landlord</a>
+    <div className="navbar border-b bg-base-100 md:px-5">
+      <div className="navbar-start">
+        <Link href={"/"} className="btn btn-ghost text-xl">
+          Rate My Landlord
+        </Link>
+      </div>
+      <div className="navbar-end space-x-2">
+        <Link href={"/post/create"} className="btn">
+          Create Post
+        </Link>
+        <button className="btn">Login</button>
+      </div>
     </div>
   );
 };

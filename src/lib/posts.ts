a17aae0046b7081f721a706c9205f0bac700da17
xilaluna <xilaluna2@gmail.com>
2024-01-07 00:14:56 -0800

    edit zod in create post

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,15 +1,15 @@
 'use server';
 
 import { db } from "~/lib/prisma";
-// import { z  } from "zod";
+import { z  } from "zod";
 
-// const PostSchema = z.object({
-//   id: z.string(),
-//   customerId: z.string(),
-//   amount: z.coerce.number(),
-//   status: z.enum(['pending', 'paid']),
-//   date: z.string(),
-// });
+const PostSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
 
 export async function getPosts() {
   const posts = await db.post.findMany();
@@ -17,13 +17,19 @@ export async function getPosts() {
 };
 
 export async function createPost(formData: FormData ){
-  const rawFormData = {
-    streetAddress: formData.get('streetAddress'),
-    city: formData.get('city'),
-    state: formData.get('state'),
-    postalCode: formData.get('postalCode'),
-    title: formData.get('title'),
-    content: formData.get('content'),
-  };
-  console.log(rawFormData);
+  try {
+    const rawFormData = {
+      streetAddress: formData.get('streetAddress'),
+      city: formData.get('city'),
+      state: formData.get('state'),
+      postalCode: formData.get('postalCode'),
+      title: formData.get('title'),
+      content: formData.get('content'),
+    };
+    console.log(rawFormData);
+    
+  } catch (error) {
+    console.error('Error creating user:', error);
+    throw error;
+  }
 }

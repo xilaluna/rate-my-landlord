b5435df33f79e284f1b73ffb960be115a22b0ab3
xilaluna <55565187+xilaluna@users.noreply.github.com>
2024-01-11 15:58:16 -0800

    custom route for clerk

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,4 +3,7 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.tsx"],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["emerald"],
+  },
 } satisfies Config;

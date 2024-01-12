24ff0f85283d4c9073ecaf89179215cad279ed1c
xilaluna <55565187+xilaluna@users.noreply.github.com>
2024-01-11 18:03:42 -0800

    remove daisy ui and start using shadcn

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,32 +1,39 @@
 import Link from "next/link";
 import { UserButton, auth, SignInButton } from "@clerk/nextjs";
+import { Button } from "~/components/ui/button";
 
 export default function Navbar() {
   const { userId } = auth();
   return (
-    <div>
-      <div>
-        <Link href={"/"}>Rate My Landlord</Link>
+    <nav className="border-b">
+      <div className="mx-auto flex max-w-7xl justify-between py-4">
+        <div className="flex items-center space-x-4">
+          <Link href={"/"} className="text-lg font-bold">
+            Rate My Landlord
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <LoggeInInfo userId={userId} />
+        </div>
       </div>
-      <LoggeInInfo userId={userId} />
-    </div>
+    </nav>
   );
 }
 
 function LoggeInInfo({ userId }: { userId: string | null }) {
   if (!userId) {
     return (
-      <div>
-        <SignInButton>
-          <button>Sign In</button>
-        </SignInButton>
-      </div>
+      <SignInButton>
+        <Button>Sign In</Button>
+      </SignInButton>
     );
   }
   return (
-    <div>
-      <Link href={"/post/create"}>Create Post</Link>
+    <>
+      <Link href={"/post/create"}>
+        <Button>Create Post</Button>
+      </Link>
       <UserButton afterSignOutUrl="/" />
-    </div>
+    </>
   );
 }

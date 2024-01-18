a49cc5a674546406d81e6db38efac40f0b54d59b
xilaluna <55565187+xilaluna@users.noreply.github.com>
2024-01-18 14:41:39 -0800

    protect edit posts

--- src/app/post/[id]/edit/page.tsx
+++ src/app/post/[id]/edit/page.tsx
@@ -1,4 +1,12 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 export default function EditPost() {
+  const { userId } = auth().protect();
+
+  if (!userId) {
+    redirect("/");
+  }
   return (
     <div>
       <h1 className="text-2xl font-bold">EditPost</h1>
